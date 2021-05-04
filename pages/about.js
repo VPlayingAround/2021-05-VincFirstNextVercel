049b6b6f94116bac3b7e40e40b222efd149389eb
Vinc <vincvinc.fa.ce.bo.ok@gmail.com>
2021-05-04 20:12:06 +0200

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,9 @@ import Head from "next/head"
 import styles from "../styles/Home.module.css"
 
 export default function About() {
+  state = {
+    count: 0,
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +16,8 @@ export default function About() {
         <h1 className={styles.title}>About</h1>
 
         <p className={styles.description}>Deze pagina weet wassup.</p>
+        <span>{this.state.count}</span>
+        <button>++!</button>
       </main>
     </div>
   )
