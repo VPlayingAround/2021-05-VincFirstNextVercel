1ea05c1ded8d9ac0e9c52f35e56e957f2c27b70e
Vinc <vincvinc.fa.ce.bo.ok@gmail.com>
2021-05-04 19:58:06 +0200

    stijl enzo er ook in

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,19 @@
+import Head from "next/head"
+import styles from "../styles/Home.module.css"
+
 export default function About() {
   return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>About</h1>
+    <div className={styles.container}>
+      <Head>
+        <title>vinc test en speelt met next en deployt met vercel</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-      <p className={styles.description}>Deze pagina weet wassup.</p>
-    </main>
+      <main className={styles.main}>
+        <h1 className={styles.title}>About</h1>
+
+        <p className={styles.description}>Deze pagina weet wassup.</p>
+      </main>
+    </div>
   )
 }
